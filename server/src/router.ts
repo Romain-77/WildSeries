deeb9e679b007877b444b35cd3ccbfb1a7dd2d93
Romain Debas <debasromain@gmail.com>
2025-12-15 14:57:54 +0100

    modify programActions, router.ts and sayActions, and also add the categoryActions on server

--- server/src/router.ts
+++ server/src/router.ts
@@ -14,12 +14,18 @@ router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
 
 /* ************************************************************************* */
-import sayActions from "./modules/say/sayActions";
-
-router.get("/", sayActions.sayWelcome);
-
 import programActions from "./modules/program/programActions";
 
 router.get("/api/programs", programActions.browse);
+router.get("/api/programs/:id", programActions.read);
+
+import categoryActions from "./modules/category/categoryActions";
+
+router.get("/api/categories", categoryActions.browse);
+router.get("/api/categories/:id", categoryActions.read);
+
+import sayActions from "./modules/say/sayActions";
+
+router.get("/", sayActions.sayWelcome);
 
 export default router;
